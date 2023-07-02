58d092c57177fb8040be40d550f8b84966aca617
higebobo <higebobo@gmail.com>
2023-07-02 15:49:47 +0900

    rc

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ CUR_DIR = os.getcwd()
 
 
 def github(suffix):
-    github_org = 'toyorepo-github'
+    # github_org = 'toyorepo-github'
+    github_org = 'toyorepo-github2'
     endpoint = 'https://api.github.com/user/repos'
     token = os.getenv(f'GITHUB_TOKEN{suffix}')
     headers = {
@@ -28,14 +29,10 @@ def github(suffix):
 
     for x in response.json():
         name = x['name']
+        description = x['description']
         url = x['ssh_url']
         visibility = x['visibility']
-        output(url, name, visibility, github_org)
-        # print(f'git clone --bare {url} {WORK_DIR}/{name}')
-        # print(f'cd {WORK_DIR}/{name}')
-        # print(f'gh repo create {github_org}/{name} --{visibility}')
-        # print(f'git push --mirror git@github.com:{github_org}/{name}')
-        # print(f'cd {CUR_DIR}')
+        output(url, description, url, description, visibility)
 
 
 def gitlab(page=100):
@@ -53,20 +50,39 @@ def gitlab(page=100):
 
     for x in response.json():
         name = x['name']
+        description = x['description']
         url = x['ssh_url_to_repo']
         visibility = x['visibility']
-        output(url, name, visibility, github_org)
+        output(url, name, visibility, description, github_org)
 
 
-def bitbucket():
+def bitbucket(pagelen=100):
     github_org = 'toyorepo-bitbucket'
-    print(github_org)
+
+    username = os.getenv('BITBUCKET_USERNAME')
+    password = os.getenv('BITBUCKET_PASSWORD')
+    endpoint = os.getenv('BITBUCKET_ENDPOINT')
+    base_url = 'https://bitbucket.org/'
+
+    session = requests.Session()
+    session.auth = (username, password)
+    params = {'page': 1, 'pagelen': pagelen}
+    resp = session.get(endpoint, params=params)
+    raw = resp.json()
+    values = raw['values']
+
+    for x in values:
+        name = x['name']
+        description = x['description']
+        visibility = 'private' if x['is_private'] else 'public'
+        url = x['links']['clone'][1]['href']
+        output(url, name, visibility, description, github_org)
 
 
-def output(url, name, visibility, github_org):
+def output(url, name, visibility, description, github_org):
     print(f'git clone --bare {url} {WORK_DIR}/{name}')
     print(f'cd {WORK_DIR}/{name}')
-    print(f'gh repo create {github_org}/{name} --{visibility}')
+    print(f'gh repo create {github_org}/{name} --{visibility} --description="{description}"')
     print(f'git push --mirror git@github.com:{github_org}/{name}')
     print(f'cd {CUR_DIR}')
 
